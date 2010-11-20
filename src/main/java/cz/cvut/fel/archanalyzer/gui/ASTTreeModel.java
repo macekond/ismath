c021b849d5c1134bbae6c77f188ab9e1b1732d65
Martin Vejmelka <vejmema1@fel.cvut.cz>
2010-11-20 15:22:26 +0100

    Added new gui package.

---
+++ src/main/java/cz/cvut/fel/archanalyzer/gui/ASTTreeModel.java
@@ -0,0 +1,58 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package cz.cvut.fel.archanalyzer.gui;
+
+import javax.swing.event.TreeModelListener;
+import javax.swing.tree.TreeModel;
+import javax.swing.tree.TreePath;
+
+/**
+ *
+ * @author martin
+ */
+public class ASTTreeModel implements TreeModel {
+
+    @Override
+    public Object getRoot() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public Object getChild(Object parent, int index) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public int getChildCount(Object parent) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public boolean isLeaf(Object node) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public void valueForPathChanged(TreePath path, Object newValue) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public int getIndexOfChild(Object parent, Object child) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public void addTreeModelListener(TreeModelListener l) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public void removeTreeModelListener(TreeModelListener l) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+}
