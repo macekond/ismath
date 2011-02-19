be6a5780e38c3714707f379e277052098b79ff28
Martin Vejmelka <vejmema1@fel.cvut.cz>
2011-02-19 22:34:24 +0100

    Added new module which sould serve as project parser.

---
+++ archval/archval/input_processor/src/main/java/fel/cvut/cz/archval/input_processor/ValidateDesignAction.java
@@ -0,0 +1,15 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fel.cvut.cz.archval.input_processor;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public final class ValidateDesignAction implements ActionListener {
+
+    public void actionPerformed(ActionEvent e) {
+        // TODO implement action body
+    }
+}
