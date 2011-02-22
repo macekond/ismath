dc8e3e5bac9c5a60160fd46e820ab9bbf570a8d2
Martin Vejmelka <vejmema1@fel.cvut.cz>
2011-02-22 13:41:55 +0100

    Replaced angle braces with parenthesis because JavaDoc treated e-mail address as a html tag.

--- archval/archval/input_processor/src/test/java/fel/cvut/cz/archval/input_processor/JavaFilesLocatorTest.java
+++ archval/archval/input_processor/src/test/java/fel/cvut/cz/archval/input_processor/JavaFilesLocatorTest.java
@@ -1,7 +1,7 @@
 /*
  *  The MIT License
  *
- *  Copyright 2011 Martin Vejmelka <martin.vejmelka@fel.cvut.cz>.
+ *  Copyright 2011 Martin Vejmelka (martin.vejmelka@fel.cvut.cz).
  *
  *  Permission is hereby granted, free of charge, to any person obtaining a copy
  *  of this software and associated documentation files (the "Software"), to deal
@@ -37,7 +37,7 @@ import org.openide.util.Exceptions;
 
 /**
  *
- * @author Martin Vejmelka <martin.vejmelka@fel.cvut.cz>
+ * @author Martin Vejmelka (martin.vejmelka@fel.cvut.cz)
  */
 public class JavaFilesLocatorTest {
 
