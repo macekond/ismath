a3b52a2354da8823eba71f3cb90286b8927082dc
Martin Vejmelka <vejmema1@fel.cvut.cz>
2011-03-04 16:30:52 +0100

    Added some fields and package declarations to testing java source file.

--- archval/archval/input_processor/src/test/resources/fel/cvut/cz/archval/input_processor/helloworld/HelloWorld.java
+++ archval/archval/input_processor/src/test/resources/fel/cvut/cz/archval/input_processor/helloworld/HelloWorld.java
@@ -1,6 +1,11 @@
 package helloworld;
 
+import java.io.DataOutputStream;
+
 public class HelloWorld {
+
+    private int testVar;
+    private DataOutputStream os;
 
     public static void main(String args[]) {
 	System.out.println("helloworld");
