5118cc8ea5328a09001726db3e5c693f19bc9820
Martin Vejmelka <vejmema1@fel.cvut.cz>
2011-04-26 15:18:23 +0200

    Add basic version of validation rules format grammar

---
+++ archval/nbarchval/testing/src/cz/cvut/fel/archval/testing/MyIface.java
@@ -0,0 +1,18 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package cz.cvut.fel.archval.testing;
+
+/**
+ *
+ * @author martin
+ */
+public interface MyIface {
+
+    public static MyIface getInstance() {
+        
+    }
+
+}
---
+++ archval/nbarchval/testing/src/cz/cvut/fel/archval/testing/grammar.g
@@ -0,0 +1,11 @@
+
+rule_specification:
+    atomic_rules
+    compound_rules
+    analyse_command
+    validate_command
+;
+
+atomic_rules:
+
+;
