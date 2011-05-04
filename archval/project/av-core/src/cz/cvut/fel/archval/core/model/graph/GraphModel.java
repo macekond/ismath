4fdf8605279d02a490cbdd88d1ce925684c652ea
Martin Vejmelka <vejmema1@fel.cvut.cz>
2011-05-04 21:42:53 +0200

    Add files for basic domain objects

---
+++ archval/project/av-core/src/cz/cvut/fel/archval/core/model/graph/GraphModel.java
@@ -0,0 +1,9 @@
+package cz.cvut.fel.archval.core.model.graph;
+
+/**
+ *
+ * @author martin
+ */
+public class GraphModel {
+
+}
---
+++ archval/project/av-core/src/cz/cvut/fel/archval/core/model/validation/ValidationModel.java
@@ -0,0 +1,9 @@
+package cz.cvut.fel.archval.core.model.validation;
+
+/**
+ *
+ * @author martin
+ */
+public class ValidationModel {
+
+}
