d77337a9d8d7a9fe6f8cc63766a939c024ebd687
Martin Vejmelka <vejmema1@fel.cvut.cz>
2011-05-08 20:38:06 +0200

    Implement some more java AST visitor methods

--- archval/project/av-graphgen-demeter/src/cz/cvut/fel/archval/demeter/graphgen/DemeterGraphGenerator.java
+++ archval/project/av-graphgen-demeter/src/cz/cvut/fel/archval/demeter/graphgen/DemeterGraphGenerator.java
@@ -1,6 +1,55 @@
 package cz.cvut.fel.archval.demeter.graphgen;
 
+import com.sun.source.tree.AnnotatedTypeTree;
+import com.sun.source.tree.AnnotationTree;
+import com.sun.source.tree.ArrayAccessTree;
+import com.sun.source.tree.ArrayTypeTree;
+import com.sun.source.tree.AssertTree;
+import com.sun.source.tree.AssignmentTree;
+import com.sun.source.tree.BinaryTree;
+import com.sun.source.tree.BlockTree;
+import com.sun.source.tree.BreakTree;
+import com.sun.source.tree.CaseTree;
+import com.sun.source.tree.CatchTree;
 import com.sun.source.tree.ClassTree;
+import com.sun.source.tree.CompilationUnitTree;
+import com.sun.source.tree.CompoundAssignmentTree;
+import com.sun.source.tree.ConditionalExpressionTree;
+import com.sun.source.tree.ContinueTree;
+import com.sun.source.tree.DoWhileLoopTree;
+import com.sun.source.tree.EmptyStatementTree;
+import com.sun.source.tree.EnhancedForLoopTree;
+import com.sun.source.tree.ErroneousTree;
+import com.sun.source.tree.ExpressionStatementTree;
+import com.sun.source.tree.ForLoopTree;
+import com.sun.source.tree.IdentifierTree;
+import com.sun.source.tree.IfTree;
+import com.sun.source.tree.ImportTree;
+import com.sun.source.tree.InstanceOfTree;
+import com.sun.source.tree.LabeledStatementTree;
+import com.sun.source.tree.LiteralTree;
+import com.sun.source.tree.MemberSelectTree;
+import com.sun.source.tree.MethodInvocationTree;
+import com.sun.source.tree.MethodTree;
+import com.sun.source.tree.ModifiersTree;
+import com.sun.source.tree.NewArrayTree;
+import com.sun.source.tree.NewClassTree;
+import com.sun.source.tree.ParameterizedTypeTree;
+import com.sun.source.tree.ParenthesizedTree;
+import com.sun.source.tree.PrimitiveTypeTree;
+import com.sun.source.tree.ReturnTree;
+import com.sun.source.tree.SwitchTree;
+import com.sun.source.tree.SynchronizedTree;
+import com.sun.source.tree.ThrowTree;
+import com.sun.source.tree.Tree;
+import com.sun.source.tree.TryTree;
+import com.sun.source.tree.TypeCastTree;
+import com.sun.source.tree.TypeParameterTree;
+import com.sun.source.tree.UnaryTree;
+import com.sun.source.tree.VariableTree;
+import com.sun.source.tree.WhileLoopTree;
+import com.sun.source.tree.WildcardTree;
+import com.sun.source.util.TreePath;
 import com.sun.source.util.TreePathScanner;
 import cz.cvut.fel.archval.core.api.graphgen.GraphGeneratorIface;
 import cz.cvut.fel.archval.core.api.model.graph.Graph;
@@ -11,6 +60,7 @@ import java.util.concurrent.ExecutionException;
 import java.util.concurrent.Future;
 import javax.lang.model.element.Element;
 import javax.lang.model.element.ElementKind;
+import javax.lang.model.element.TypeElement;
 import org.netbeans.api.java.source.CompilationController;
 import org.netbeans.api.java.source.CompilationInfo;
 import org.netbeans.api.java.source.JavaSource;
@@ -18,7 +68,6 @@ import org.netbeans.api.java.source.Task;
 import org.openide.awt.StatusDisplayer;
 import org.openide.filesystems.FileObject;
 import org.openide.filesystems.FileUtil;
-import org.openide.util.Exceptions;
 import org.openide.windows.IOProvider;
 import org.openide.windows.InputOutput;
 
@@ -36,9 +85,6 @@ public class DemeterGraphGenerator implements GraphGeneratorIface {
     }
 
     public Graph getGraph(File projectDirectory) throws IOException, InterruptedException, ExecutionException {
-
-        final InputOutput io = IOProvider.getDefault().getIO("DemeterGraphGen", true);
-        io.select();
 
         Graph graph = new Graph();
 
@@ -59,15 +105,17 @@ public class DemeterGraphGenerator implements GraphGeneratorIface {
             if (js == null) {
                 continue;
             }
-            processJavaSource(js, graph, io);
+            processJavaSource(js, graph);
         }
         return graph;
     }
 
-    protected void processJavaSource(JavaSource javaSource, Graph graph,
-            InputOutput io) throws IOException, InterruptedException, ExecutionException {
-        // TODO: process java source and add required elements to graph
-        io.getOut().println("Processing next file.");
+    protected void processJavaSource(JavaSource javaSource, Graph graph) throws
+            IOException, InterruptedException, ExecutionException {
+
+        // TODO: process java source and add all elements to graph
+        InputOutput io = IOProvider.getDefault().getIO("DG-analysis", false);
+        io.getOut().println();
 
         Task<CompilationController> task = new Task<CompilationController>() {
 
@@ -93,36 +141,306 @@ public class DemeterGraphGenerator implements GraphGeneratorIface {
     private static class MemberVisitor extends TreePathScanner<Void, Void> {
 
         private CompilationInfo info;
+        private InputOutput io = IOProvider.getDefault().getIO("DG-analysis", false);
 
         public MemberVisitor(CompilationInfo info) {
             this.info = info;
+            io.select();
         }
 
-        @Override
         public Void visitClass(ClassTree t, Void v) {
-            Element el = info.getTrees().getElement(getCurrentPath());
+            TreePath currentPath = getCurrentPath();
+            int level = getNestingLevel(currentPath);
+            Element el = info.getTrees().getElement(currentPath);
             if (el == null) {
                 StatusDisplayer.getDefault().setStatusText(
                         "Cannot resolve class!");
             } else {
-                InputOutput io = IOProvider.getDefault().getIO("Analysis of "
-                        + info.getFileObject().getName(), true);
-                if (el.getKind() == ElementKind.CONSTRUCTOR) {
-                    io.getOut().println("Hurray, this is a constructor: "
-                            + el.getSimpleName());
-                } else if (el.getKind() == ElementKind.METHOD) {
-                    io.getOut().println("Hurray, this is a method: "
-                            + el.getSimpleName());
-                } else if (el.getKind() == ElementKind.FIELD) {
-                    io.getOut().println("Hurray, this is a field: "
-                            + el.getSimpleName());
-                } else {
-                    io.getOut().println("Hurray, this is something else: "
-                            + el.getSimpleName());
+                if (el.getKind() == ElementKind.CLASS || el.getKind() == ElementKind.INTERFACE) {
+                    TypeElement typeElement = (TypeElement) el;
+                    printIndented("class name: " + typeElement.getQualifiedName(), level);
+                } else if (el.getKind() == ElementKind.ENUM) {
+                    TypeElement typeElement = (TypeElement) el;
+                    printIndented("enum name: " + typeElement.getQualifiedName(), level);
                 }
-                io.getOut().close();
             }
-            return null;
+            return super.visitClass(t, v);
+        }
+
+        @Override
+        public Void visitMethod(MethodTree node, Void p) {
+            TreePath currentPath = getCurrentPath();
+            int level = getNestingLevel(currentPath);
+            printIndented("method: " + node.getName(), level);
+            return super.visitMethod(node, p);
+        }
+
+        @Override
+        public Void visitIdentifier(IdentifierTree node, Void p) {
+            TreePath currentPath = getCurrentPath();
+            int level = getNestingLevel(currentPath);
+            Element el = info.getTrees().getElement(currentPath);
+            if (el.getKind() == ElementKind.CLASS || el.getKind() == ElementKind.INTERFACE) {
+                TypeElement typeElement = (TypeElement) el;
+                printIndented("use of type: " + typeElement.getQualifiedName(), level);
+            }
+            return super.visitIdentifier(node, p);
+        }
+
+        @Override
+        public Void visitVariable(VariableTree node, Void p) {
+            TreePath currentPath = getCurrentPath();
+            int level = getNestingLevel(currentPath);
+            Element el = info.getTrees().getElement(currentPath);
+            printIndented("variable: " + el.getSimpleName(), level);
+
+            if (el.getKind() == ElementKind.CLASS || el.getKind() == ElementKind.INTERFACE) {
+                TypeElement typeElement = (TypeElement) el;
+                printIndented("variable type: " + typeElement.getQualifiedName(), level);
+            }
+            return super.visitVariable(node, p);
+        }
+
+        @Override
+        public Void visitAnnotatedType(AnnotatedTypeTree node, Void p) {
+            return super.visitAnnotatedType(node, p);
+        }
+
+        @Override
+        public Void visitAnnotation(AnnotationTree node, Void p) {
+            return super.visitAnnotation(node, p);
+        }
+
+        @Override
+        public Void visitArrayAccess(ArrayAccessTree node, Void p) {
+            return super.visitArrayAccess(node, p);
+        }
+
+        @Override
+        public Void visitArrayType(ArrayTypeTree node, Void p) {
+            return super.visitArrayType(node, p);
+        }
+
+        @Override
+        public Void visitAssert(AssertTree node, Void p) {
+            return super.visitAssert(node, p);
+        }
+
+        @Override
+        public Void visitAssignment(AssignmentTree node, Void p) {
+            return super.visitAssignment(node, p);
+        }
+
+        @Override
+        public Void visitBinary(BinaryTree node, Void p) {
+            return super.visitBinary(node, p);
+        }
+
+        @Override
+        public Void visitBlock(BlockTree node, Void p) {
+            return super.visitBlock(node, p);
+        }
+
+        @Override
+        public Void visitBreak(BreakTree node, Void p) {
+            return super.visitBreak(node, p);
+        }
+
+        @Override
+        public Void visitCase(CaseTree node, Void p) {
+            return super.visitCase(node, p);
+        }
+
+        @Override
+        public Void visitCatch(CatchTree node, Void p) {
+            return super.visitCatch(node, p);
+        }
+
+        @Override
+        public Void visitCompilationUnit(CompilationUnitTree node, Void p) {
+            return super.visitCompilationUnit(node, p);
+        }
+
+        @Override
+        public Void visitCompoundAssignment(CompoundAssignmentTree node, Void p) {
+            return super.visitCompoundAssignment(node, p);
+        }
+
+        @Override
+        public Void visitConditionalExpression(ConditionalExpressionTree node, Void p) {
+            return super.visitConditionalExpression(node, p);
+        }
+
+        @Override
+        public Void visitContinue(ContinueTree node, Void p) {
+            return super.visitContinue(node, p);
+        }
+
+        @Override
+        public Void visitDoWhileLoop(DoWhileLoopTree node, Void p) {
+            return super.visitDoWhileLoop(node, p);
+        }
+
+        @Override
+        public Void visitEmptyStatement(EmptyStatementTree node, Void p) {
+            return super.visitEmptyStatement(node, p);
+        }
+
+        @Override
+        public Void visitEnhancedForLoop(EnhancedForLoopTree node, Void p) {
+            return super.visitEnhancedForLoop(node, p);
+        }
+
+        @Override
+        public Void visitErroneous(ErroneousTree node, Void p) {
+            return super.visitErroneous(node, p);
+        }
+
+        @Override
+        public Void visitExpressionStatement(ExpressionStatementTree node, Void p) {
+            return super.visitExpressionStatement(node, p);
+        }
+
+        @Override
+        public Void visitForLoop(ForLoopTree node, Void p) {
+            return super.visitForLoop(node, p);
+        }
+
+        @Override
+        public Void visitIf(IfTree node, Void p) {
+            return super.visitIf(node, p);
+        }
+
+        @Override
+        public Void visitImport(ImportTree node, Void p) {
+            return super.visitImport(node, p);
+        }
+
+        @Override
+        public Void visitInstanceOf(InstanceOfTree node, Void p) {
+            return super.visitInstanceOf(node, p);
+        }
+
+        @Override
+        public Void visitLabeledStatement(LabeledStatementTree node, Void p) {
+            return super.visitLabeledStatement(node, p);
+        }
+
+        @Override
+        public Void visitLiteral(LiteralTree node, Void p) {
+            return super.visitLiteral(node, p);
+        }
+
+        @Override
+        public Void visitMemberSelect(MemberSelectTree node, Void p) {
+            return super.visitMemberSelect(node, p);
+        }
+
+        @Override
+        public Void visitMethodInvocation(MethodInvocationTree node, Void p) {
+            return super.visitMethodInvocation(node, p);
+        }
+
+        @Override
+        public Void visitModifiers(ModifiersTree node, Void p) {
+            return super.visitModifiers(node, p);
+        }
+
+        @Override
+        public Void visitNewArray(NewArrayTree node, Void p) {
+            return super.visitNewArray(node, p);
+        }
+
+        @Override
+        public Void visitNewClass(NewClassTree node, Void p) {
+            return super.visitNewClass(node, p);
+        }
+
+        @Override
+        public Void visitOther(Tree node, Void p) {
+            return super.visitOther(node, p);
+        }
+
+        @Override
+        public Void visitParameterizedType(ParameterizedTypeTree node, Void p) {
+            return super.visitParameterizedType(node, p);
+        }
+
+        @Override
+        public Void visitParenthesized(ParenthesizedTree node, Void p) {
+            return super.visitParenthesized(node, p);
+        }
+
+        @Override
+        public Void visitPrimitiveType(PrimitiveTypeTree node, Void p) {
+            return super.visitPrimitiveType(node, p);
+        }
+
+        @Override
+        public Void visitReturn(ReturnTree node, Void p) {
+            return super.visitReturn(node, p);
+        }
+
+        @Override
+        public Void visitSwitch(SwitchTree node, Void p) {
+            return super.visitSwitch(node, p);
+        }
+
+        @Override
+        public Void visitSynchronized(SynchronizedTree node, Void p) {
+            return super.visitSynchronized(node, p);
+        }
+
+        @Override
+        public Void visitThrow(ThrowTree node, Void p) {
+            return super.visitThrow(node, p);
+        }
+
+        @Override
+        public Void visitTry(TryTree node, Void p) {
+            return super.visitTry(node, p);
+        }
+
+        @Override
+        public Void visitTypeCast(TypeCastTree node, Void p) {
+            return super.visitTypeCast(node, p);
+        }
+
+        @Override
+        public Void visitTypeParameter(TypeParameterTree node, Void p) {
+            return super.visitTypeParameter(node, p);
+        }
+
+        @Override
+        public Void visitUnary(UnaryTree node, Void p) {
+            return super.visitUnary(node, p);
+        }
+
+        @Override
+        public Void visitWhileLoop(WhileLoopTree node, Void p) {
+            return super.visitWhileLoop(node, p);
+        }
+
+        @Override
+        public Void visitWildcard(WildcardTree node, Void p) {
+            return super.visitWildcard(node, p);
+        }
+
+        private int getNestingLevel(TreePath treePath) {
+            int level = 0;
+            TreePath tp = treePath;
+            while ((tp = tp.getParentPath()) != null) {
+                level++;
+            }
+            return level;
+        }
+
+        private void printIndented(String string, int level) {
+            String indentation = "";
+            for (int i = 0; i < level; i++) {
+                indentation += "    ";
+            }
+            io.getOut().println(indentation + string);
         }
     }
 }
