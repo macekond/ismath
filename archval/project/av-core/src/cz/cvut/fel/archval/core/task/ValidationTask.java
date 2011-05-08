489de7f8be503da43bc3df248bcb7f025dd27a84
Martin Vejmelka <vejmema1@fel.cvut.cz>
2011-05-08 11:10:04 +0200

    Implement validation task

--- archval/project/av-core/src/cz/cvut/fel/archval/core/task/ValidationTask.java
+++ archval/project/av-core/src/cz/cvut/fel/archval/core/task/ValidationTask.java
@@ -2,9 +2,12 @@ package cz.cvut.fel.archval.core.task;
 
 import cz.cvut.fel.archval.core.api.ValidationCompletedListener;
 import cz.cvut.fel.archval.core.api.ValidationTaskIface;
+import cz.cvut.fel.archval.core.api.ex.RequiredGraphNotFound;
 import cz.cvut.fel.archval.core.api.model.graph.GraphModel;
 import cz.cvut.fel.archval.core.api.model.report.ValidationReport;
 import cz.cvut.fel.archval.core.model.validation.ValidationModel;
+import java.util.LinkedList;
+import java.util.List;
 
 /**
  * Manages validation process.
@@ -13,28 +16,61 @@ import cz.cvut.fel.archval.core.model.validation.ValidationModel;
  */
 public class ValidationTask implements ValidationTaskIface {
 
+    private boolean taskRunning = false;
+    private boolean validationPerformed;
+    private GraphModel graphModel;
+    private ValidationModel validationModel;
+    private ValidationReport validationReport;
+    private List<ValidationCompletedListener> validationCompletedListeners;
+    private Exception caughtException;
+
     public ValidationTask(GraphModel graphModel, ValidationModel validationModel) {
-        // TODO: implement
-        throw new UnsupportedOperationException("Not supported yet.");
+        this.graphModel = graphModel;
+        this.validationModel = validationModel;
+        validationCompletedListeners = new LinkedList<ValidationCompletedListener>();
+        caughtException = null;
+        validationPerformed = false;
     }
 
     public void runSynchronous() {
-        // TODO: implement
-        throw new UnsupportedOperationException("Not supported yet.");
+        try {
+            validationModel.validate(graphModel);
+        } catch (RequiredGraphNotFound ex) {
+            caughtException = ex;
+        }
+        validationPerformed = true;
     }
 
-    public void runAsynchronous() {
-        // TODO: implement
-        throw new UnsupportedOperationException("Not supported yet.");
+    public synchronized void runAsynchronous() {
+        if (taskRunning == true) {
+            throw new IllegalStateException("It's illegal to run one validation"
+                    + " task more then once.");
+        }
+        taskRunning = true;
+        new Thread(new Runnable() {
+
+            public void run() {
+                runSynchronous();
+                fireValidationCompletedEvent();
+            }
+        }).start();
     }
 
     public void registerValidationCompletedListener(ValidationCompletedListener validationCompletedListener) {
-        // TODO: implement
-        throw new UnsupportedOperationException("Not supported yet.");
+        validationCompletedListeners.add(validationCompletedListener);
     }
 
     public ValidationReport getReport() {
-        // TODO: implement
-        throw new UnsupportedOperationException("Not supported yet.");
+        if (validationPerformed == false) {
+            throw new IllegalStateException("You can't call this method before"
+                    + " one of run* methods.");
+        }
+        return validationReport;
+    }
+
+    private void fireValidationCompletedEvent() {
+        for (ValidationCompletedListener validationCompletedListener : validationCompletedListeners) {
+            validationCompletedListener.validationCompleted(caughtException);
+        }
     }
 }
