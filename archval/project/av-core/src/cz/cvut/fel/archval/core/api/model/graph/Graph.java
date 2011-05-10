05ee315bcfefcdf55e69a3cbeae8818c61a76bba
Martin Vejmelka <vejmema1@fel.cvut.cz>
2011-05-10 06:53:45 +0200

    Modify graph for better access by name, realize FVertexSelector

--- archval/project/av-core/src/cz/cvut/fel/archval/core/api/model/graph/Graph.java
+++ archval/project/av-core/src/cz/cvut/fel/archval/core/api/model/graph/Graph.java
@@ -1,7 +1,10 @@
 package cz.cvut.fel.archval.core.api.model.graph;
 
 import cz.cvut.fel.archval.core.api.model.graph.visitor.GraphElementVisitor;
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 /**
  * Represents one graph generated for software project sources.
@@ -10,9 +13,16 @@ import java.util.List;
  */
 public class Graph {
 
-    private List<Edge> edges;
-    private List<Vertex> vertices;
+    private Set<Edge> edges;
+    private Set<Vertex> vertices;
     private String graphType;
+    private HashMap<String, Vertex> vertexNameMap;
+
+    public Graph() {
+        edges = new HashSet<Edge>();
+        vertices = new HashSet<Vertex>();
+        vertexNameMap = new HashMap<String, Vertex>();
+    }
 
     public String getGraphType() {
         return graphType;
@@ -22,12 +32,50 @@ public class Graph {
         this.graphType = graphType;
     }
 
-    public List<Vertex> getVertices() {
+    @Deprecated
+    public Set<Vertex> getVertices() {
         return vertices;
     }
 
-    public List<Edge> getEdges() {
+    @Deprecated
+    public Set<Edge> getEdges() {
         return edges;
+    }
+
+    public void addEdge(Edge edge) {
+        if (!vertices.contains(edge.getHead())
+                || !vertices.contains(edge.getTail())) {
+                throw new IllegalArgumentException("You can add to the graph"
+                        + "only edges joining existing elements.");
+        }
+        edges.add(edge);
+    }
+
+    public void addVertex(Vertex vertex) {
+        vertices.add(vertex);
+        if (vertexNameMap.containsKey(vertex.getName())) {
+            return;
+        }
+        vertexNameMap.put(vertex.getName(), vertex);
+    }
+
+    public Set<Edge> getVertexOutgoingEdges(Vertex vertex) {
+        Set<Edge> resultSet = new HashSet<Edge>();
+        for (Edge edge : resultSet) {
+            if (edge.getTail().equals(vertex)) {
+                resultSet.add(edge);
+            }
+        }
+        return resultSet;
+    }
+
+    /**
+     * Returns vertex by its name or null if no such vertex is in the graph
+     * @param name name of the vertex to be found
+     * @return Vertex instance or null
+     */
+    public Vertex getVertexByName(String name) {
+        return vertexNameMap.get(name);
     }
 
     /**
--- archval/project/av-operators-demeter/src/cz/cvut/fel/archval/demeter/operators/FVertexSetSelector.java
+++ archval/project/av-operators-demeter/src/cz/cvut/fel/archval/demeter/operators/FVertexSetSelector.java
@@ -1,11 +1,14 @@
 package cz.cvut.fel.archval.demeter.operators;
 
+import cz.cvut.fel.archval.core.api.model.graph.Edge;
 import cz.cvut.fel.archval.core.api.model.graph.Graph;
 import cz.cvut.fel.archval.core.api.model.graph.Vertex;
 import cz.cvut.fel.archval.core.api.operator.OperatorIface;
 import cz.cvut.fel.archval.core.api.types.DataType;
+import java.util.HashSet;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Set;
 
 /**
  * F selector implementation (selects vertices which are first follower with
@@ -44,8 +47,42 @@ public class FVertexSetSelector implements OperatorIface {
         Vertex vertex = (Vertex) operands.get(0);
         String vertexLabel = (String) operands.get(1);
         String edgeLabel = (String) operands.get(2);
-        
+
+        Set<Vertex> resultSet = new HashSet<Vertex>();
+        Set<Vertex> visitedVertices = new HashSet<Vertex>();
+        collectVertices(graph, vertex, vertexLabel, edgeLabel, resultSet,
+                false, visitedVertices);
+
         // TODO: implement F operator as specified in the work
         throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    private void collectVertices(Graph g, Vertex v, String kind,
+            String classifier, Set<Vertex> result, boolean labelFound,
+            Set<Vertex> visitedVertices) {
+
+        if (visitedVertices.contains(v)) {
+            return; // this vertex was already visited
+        }
+
+        visitedVertices.add(v); // add this vertex to visited
+
+        // if this vertex has required kind and was accessed using path
+        // containing required label, add it to the resultset and return
+        if (v.getKind().equals(kind) && labelFound) {
+            result.add(v);
+            return;
+        }
+
+        // process outgoing edges
+        for (Edge edge : g.getVertexOutgoingEdges(v)) {
+            if (edge.getClassifier().equals(classifier)) {
+                collectVertices(g, v, kind, classifier, result, true,
+                        visitedVertices);
+            } else {
+                collectVertices(g, v, kind, classifier, result, false,
+                        visitedVertices);
+            }
+        }
     }
 }
