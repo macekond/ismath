f6dfaa673781327c9619e6c4ce6723c3dad130f2
Martin Vejmelka <vejmema1@fel.cvut.cz>
2011-05-12 07:11:22 +0200

    Remove too complicated samples

--- archval/samples/cvut_malekji3/sw/src/main/java/cz/cvut/fel/malekji3/sw/bank/AccountException.java
+++
@@ -1,9 +0,0 @@
-package cz.cvut.fel.malekji3.sw.bank;
-
-class AccountException extends Exception
-{
-    public AccountException(String message)
-    {
-        super(message);
-    }
-}
--- archval/samples/cvut_watzkdav/task1/src/main/java/cz/cvut/fel/watzkdav/task1/Homework1.java
+++
@@ -1,9 +0,0 @@
-package cz.cvut.fel.watzkdav.task1;
-
-public class Homework1 {
-	private int count = 0;
-
-	public boolean f() { return true; }
-	public boolean g() { return false; }
-	public int h() { return ++count; }
-}
