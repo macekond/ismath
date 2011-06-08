3be6f69cb9c24ccf270f31b7b182f61daa9f9dd6
Martin Vejmelka <vejmema1@fel.cvut.cz>
2011-06-08 20:35:02 +0200

    Add rule processing demonstration

---
+++ archval/presentation/graphs/build.sh
@@ -0,0 +1,4 @@
+dot -Tpng graph_example1.dot -o graph_example1.png
+dot -Tpng graph_example2.dot -o graph_example2.png
+dot -Tpng graph_example3.dot -o graph_example3.png
+dot -Tpng graph_example4.dot -o graph_example4.png
